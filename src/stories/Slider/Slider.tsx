d5490838a80454278aa0557e6973c9def663e4e6
Khang Le Thanh <khangthanh2312@gmail.com>
2025-11-25 14:30:34 +0200

    modifying css slider

--- src/stories/Slider/Slider.tsx
+++ src/stories/Slider/Slider.tsx
@@ -13,38 +13,38 @@ export interface SliderProps {
 /** Primary UI component for user interaction */
 const Slider: React.FC<SliderProps> = ({
   title,
-  disabled,
+  disabled = false,
   onChange,
   min,
   max,
   value,
   ...props
 }) => {
-  const id = `slider-${title.toString().replace(/\s+/g, "-").toLowerCase()}`;
+  const id = `slider-${title.toLowerCase().replace(/\s+/g, "-")}`;
 
   return (
-    <>
-      <label htmlFor={id} className={`slider ${disabled ? "disabled" : ""}`}>
-        {title}: {value}{" "}
+    <div className="slider-wrapper">
+      <label htmlFor={id} className="slider-title">
+        {title}: {value}
       </label>
 
       <input
         id={id}
+        type="range"
         role="slider"
-        onChange={onChange}
-        disabled={disabled}
+        className={`slider-input ${disabled ? "disabled-slider" : ""}`}
         min={min}
         max={max}
         value={value}
+        onChange={onChange}
+        disabled={disabled}
         aria-valuemin={min}
         aria-valuemax={max}
         aria-valuenow={value}
         aria-label={title}
-        type="range"
-        className={`input ${disabled ? "disabled-slider" : "slider"}`}
         {...props}
       />
-    </>
+    </div>
   );
 };
 
